bdf4bceefc296ed0192e4bca9dfb7424b9170c06
mckinneyjaiden5418 <spiderbeary@gmail.com>
2025-10-11 17:35:08 +0000

    Finished bot setup & moved run_bot to bot.py.

--- python/main.py
+++ python/main.py
@@ -1,34 +1,24 @@
 """Main."""
-
 import os
 
-import discord
-from discord.ext import commands
+from bot.bot import run_bot
 from dotenv import load_dotenv
 from log import logger
 
 load_dotenv()
 
 def validate_env() -> bool:
-    """Validate if .env file has required variable(s)."""
+    """Validate if .env file has required variable(s).
+
+    Returns:
+        bool: True if valid env.
+    """
     required_vars: list[str] = ["DISCORD_BOT_TOKEN"]
     missing_vars: list[str] = [var for var in required_vars if not os.getenv(var)]
 
     if missing_vars:
         logger.error(f"Missing required environment variables: {', '.join(missing_vars)}")
     return not missing_vars
-
-
-def run_bot() -> None: # Move this to bot.py later.
-    """Run discord bot."""
-    token: str | None = os.getenv("DISCORD_BOT_TOKEN")
-
-    intents: discord.Intents = discord.Intents.default()
-    intents.message_content = True
-
-    bot: commands.Bot = commands.Bot(command_prefix="$", intents=intents)
-
-    bot.run(token=token) # pyright: ignore[reportArgumentType]
 
 
 def main() -> None:
