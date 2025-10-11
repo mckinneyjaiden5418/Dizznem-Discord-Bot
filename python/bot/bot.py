2373ca83b6396c4677b0de4b8670e9c1c081f21c
mckinneyjaiden5418 <spiderbeary@gmail.com>
2025-10-11 17:37:29 +0000

    Basic function to start bot.

--- python/bot/bot.py
+++ python/bot/bot.py
@@ -1,1 +1,25 @@
-"""All bot commands go here."""
+"""Main file for bot."""
+import os
+from typing import cast
+
+import discord
+from discord.ext import commands
+from log import logger
+
+
+def run_bot() -> None:
+    """Run discord bot."""
+    token: str = cast("str", os.getenv("DISCORD_BOT_TOKEN"))
+    test_channel: int = int(cast("str", os.getenv("TEST_CHANNEL_ID")))
+
+    intents: discord.Intents = discord.Intents.default()
+    intents.message_content = True
+
+    bot: commands.Bot = commands.Bot(command_prefix="$", intents=intents)
+
+    @bot.event
+    async def on_ready() -> None:
+        channel: discord.TextChannel = cast("discord.TextChannel", bot.get_channel(test_channel))
+        await channel.send("Hello")
+
+    bot.run(token=token)
