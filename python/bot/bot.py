3fbbdfb6ea70bb5320482e775001b8b21c3f9c65
mckinneyjaiden5418 <spiderbeary@gmail.com>
2025-10-12 19:51:09 +0000

    DizznemBot class & on_message command.

--- python/bot/bot.py
+++ python/bot/bot.py
@@ -2,24 +2,73 @@
 import os
 from typing import cast
 
-import discord
+from discord import Intents, Message, TextChannel
 from discord.ext import commands
 from log import logger
 
 
-def run_bot() -> None:
-    """Run discord bot."""
-    token: str = cast("str", os.getenv("DISCORD_BOT_TOKEN"))
-    test_channel: int = int(cast("str", os.getenv("TEST_CHANNEL_ID")))
+class DizznemBot(commands.Bot):
+    """Dizznem Bot class."""
 
-    intents: discord.Intents = discord.Intents.default()
-    intents.message_content = True
+    def __init__(self) -> None:
+        """Initiate Dizznem Bot config/intents."""
+        intents: Intents = Intents.default()
+        intents.message_content = True
 
-    bot: commands.Bot = commands.Bot(command_prefix="$", intents=intents)
+        super().__init__(command_prefix="$", intents=intents)
 
-    @bot.event
-    async def on_ready() -> None:
-        channel: discord.TextChannel = cast("discord.TextChannel", bot.get_channel(test_channel))
+        self.token: str = cast("str", os.getenv("DISCORD_BOT_TOKEN"))
+        self.bot_tag: str = cast("str", os.getenv("DISCORD_BOT_TAG", "dizznem"))
+        self.test_channel_id: int = int(cast("str", os.getenv("TEST_CHANNEL_ID", "0")))
+
+
+    async def on_ready(self) -> None:
+        """Bot startup."""
+        channel: TextChannel = cast("TextChannel", self.get_channel(self.test_channel_id))
         await channel.send("Hello")
 
-    bot.run(token=token)
+
+    async def on_message(self, message: Message) -> None:
+        """Handle message events.
+
+        Args:
+            message (Message): User message.
+        """
+        if message.author == self.user or message.author.bot:
+            return
+
+        # Level feature here when added.
+
+        triggers: list[tuple[str, str]] = [
+            (self.bot_tag, ""),
+            ("wackdiff", "cuckdiff*"),
+            ("wack", "cuck*"),
+            ("minor", "A MINORRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR"),
+            ("spark", "Please spark Dizznem please!!!!!!"),
+            ("dizznem", "dihhnem*"),
+            ("cook", "We need to cook."),
+            ("call", "Better Call Saul!"),
+            # ("gay", "Dizznem Bot is an LGBTQ+ ally!"),  # Vaulted pride month command
+            ("limit", "WE SAIYANS HAVE NO LIMITS!!!"),
+            ("super speed clicker", "https://www.roblox.com/games/139600379808227/Super-Speed-Clicker"),
+        ]
+
+        content: str = message.content.lower()
+        for trigger, response in triggers:
+            if trigger not in content:
+                continue
+
+            if trigger == self.bot_tag:
+                await message.channel.send("Placeholder!")
+                # AI feature here when added.
+            else:
+                await message.channel.send(response)
+
+            break
+
+        await self.process_commands(message)
+
+
+    def run_bot(self) -> None:
+        """Run bot."""
+        super().run(self.token)
