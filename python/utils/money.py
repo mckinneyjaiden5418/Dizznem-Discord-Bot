695204e05c4d090db197929b58ff7db2c7a15ccb
mckinneyjaiden5418 <spiderbeary@gmail.com>
2025-10-18 19:37:53 +0000

    Added format_money method.

---
+++ python/utils/money.py
@@ -0,0 +1,12 @@
+"""Money util."""
+def format_money(money: float) -> str:
+    """Format money.
+
+    Args:
+        money (float): Money.
+
+    Returns:
+        str: Formatted money as a readable string.
+    """
+    formatted_money: str = f"{money:,.2f}"
+    return formatted_money.removesuffix(".00")
