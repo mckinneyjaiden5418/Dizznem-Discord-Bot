69b5165bdd583a5f823b2f34c859ac7a62d3ea9d
mckinneyjaiden5418 <spiderbeary@gmail.com>
2026-01-05 04:16:01 +0000

    $setmoney command done.

---
+++ python/bot/cogs/admin.py
@@ -0,0 +1,70 @@
+"""Admin commands."""
+
+from bot.bot import DizznemBot
+from discord import Color, Embed, Member
+from discord.ext import commands
+from log import logger  # noqa: F401
+from user import User
+from utils.numbers import convert_money_str
+
+
+class Admin(commands.Cog):
+    """Admin commands."""
+
+    def __init__(self, bot: DizznemBot) -> None:
+        """Initialize Admin.
+
+        Args:
+            bot (DizznemBot): Dizznem Bot.
+        """
+        self.bot: DizznemBot = bot
+
+    @commands.hybrid_command(name="setmoney", description="Set money for a user (admin command).")
+    async def set_money(self, ctx: commands.Context, member: Member, amount: str) -> None:
+        """Set money for a user.
+
+        Args:
+            ctx (commands.Context): Context.
+            member (Member): Member to set money for.
+            amount (str): Money amount.
+        """
+        if ctx.author.id != self.bot.admin_id:
+            embed: Embed = Embed(
+                title="Error",
+                color=Color.red(),
+                description="You do not have access to this command.",
+            )
+            await ctx.send(embed=embed)
+            return
+
+        try:
+            amount_float: float = convert_money_str(money_str=amount)
+        except ValueError:
+            await ctx.send(
+                embed=Embed(
+                    title="Error",
+                    color=Color.red(),
+                    description="Invalid money format.",
+                ),
+            )
+            return
+
+        user: User = User.create_if_not_exists(user_id=member.id, username=member.name)
+        user.money = amount_float
+
+        embed: Embed = Embed(
+            title="Placeholder",
+            color=Color.green(),
+            description=f"Added {amount_float}.",
+        )
+
+        await ctx.send(embed=embed)
+
+
+async def setup(bot: DizznemBot) -> None:
+    """Setup for Admin.
+
+    Args:
+        bot (DizznemBot): Dizznem Bot.
+    """
+    await bot.add_cog(Admin(bot))
