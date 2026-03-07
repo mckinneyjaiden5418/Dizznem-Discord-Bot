8e4a11d564377a95e5593aeb9265e1b1fc7e9150
mckinneyjaiden5418 <spiderbeary@gmail.com>
2026-03-07 18:13:37 +0000

    Added get_user_answer helper function.

--- python/utils/general.py
+++ python/utils/general.py
@@ -1,4 +1,7 @@
 """General util."""
+import asyncio
+
+from discord import Message
 from discord.ext import commands
 
 
@@ -10,3 +13,22 @@ def reset_cd(ctx: commands.Context) -> None:
     """
     if ctx.command is not None:
         ctx.command.reset_cooldown(ctx)
+
+async def get_user_answer(
+    bot: commands.Bot,
+    ctx: commands.Context,
+    timeout: int = 15,
+) -> str | None:
+    """Wait for a user's message answer.
+
+    Returns:
+        str | None: User's message content or None if timed out.
+    """
+    def check(message: Message) -> bool:
+        return message.author == ctx.author and message.channel == ctx.channel
+
+    try:
+        msg: Message = await bot.wait_for("message", check=check, timeout=timeout)
+        return msg.content  # noqa: TRY300
+    except asyncio.TimeoutError:
+        return None
